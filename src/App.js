b85a9bf3fcb9a2ebb2f38cdd50744c2b3c51f6e1
Roxolana <roxolanapavlyk@gmail.com>
2019-03-03 23:43:19 +0100

    add component to router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
 import Main from './components/Main'
 import SeeList from './components/SeeList'
+import DoneTasksList from './components/DoneTasksList'
 
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <div className="App">
           <Route exact path='/' component={Main}></Route> 
           <Route path='/list' component={SeeList}></Route> 
+         <Route path='/done' component={DoneTasksList}></Route>
         </div>
       </BrowserRouter>
     )
