9416da93167503b448e4541e96a662b47753b260
Roxolana <roxolanapavlyk@gmail.com>
2019-03-03 23:45:07 +0100

    add button link to done tasks

--- src/components/SeeList.js
+++ src/components/SeeList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import TaskDelete from './TaskDelete';
+import DoneTask from './DoneTask'
 import { Link } from 'react-router-dom'
 
 
@@ -9,27 +10,30 @@ export default class SeeList extends Component {
     super()
     this.state={
       tasks:[],
-      isLoaded: false,
-      is_done:false
+      isLoaded: false
     }
   }
 
   componentDidMount(){
     axios.get(`http://127.0.0.1:8000/todo/`)
-    .then(res=>this.setState({
+    .then(res=>
+      this.setState({
       tasks:res.data,
       isLoaded:true
-    }))
+    })  
+  )
   }
-  
+ 
   render() {
     let myList = this.state.isLoaded?this.state.tasks.map(task=>{
       return(
         <li key={task.id}>{task.task_name}
+        <div className='buttons'>
         <TaskDelete id={task.id}/>
+          <DoneTask id={task.id}/>
+          </div>
         </li>
     )}):<li>Loading......</li>
-
     return (
       <div className='SeeList'>
         <div className='listbutton'>
@@ -39,6 +43,11 @@ export default class SeeList extends Component {
             </button>
           </Link>
           <h1>My list of tasks</h1>
+          <Link to='/done'>
+            <button className='myButtonBack'>
+              <i className="fas fa-chevron-right " style={{ fontSize: '2rem' }}></i>
+            </button>
+          </Link>
         </div>
         <ul className='listTasks'>
           {myList}
